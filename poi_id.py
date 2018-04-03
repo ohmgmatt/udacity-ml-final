24c414b56192d513ee809b1261dfbb780a095aea
ohmgmatt <bonilla.matthew44@gmail.com>
2018-04-02 18:33:24 -0700

    Initial classifier test

--- poi_id.py
+++ poi_id.py
@@ -7,6 +7,7 @@ sys.path.append("tools/")
 from feature_format import featureFormat, targetFeatureSplit
 from tester import dump_classifier_and_data
 from collections import Counter
+from time import time
 
 ### Task 1: Select what features you'll use.
 ### features_list is a list of strings, each of which is a feature name.
@@ -23,7 +24,19 @@ with open("final_project_dataset.pkl", "r") as data_file:
 
 
 ### Task 2: Remove outliers
-print data_dict
+#for x in range(0,len(data_dict.keys())):
+#    if data_dict[data_dict.keys()[x]]['total_stock_value'] == 'NaN':
+#        print(data_dict.keys()[x],':',data_dict[data_dict.keys()[x]]['poi'])
+data_dict.pop('TOTAL')
+
+## Deciding to set any NaN and values under 0 to 0 since they are assumed
+## to have 0 in total_stock_value
+for x in range(0, len(data_dict.keys())):
+    if data_dict[data_dict.keys()[x]]['total_stock_value'] == 'NaN' or \
+    data_dict[data_dict.keys()[x]]['total_stock_value'] <= 0:
+        data_dict[data_dict.keys()[x]]['total_stock_value'] = 0
+
+
 
 ### Task 3: Create new feature(s)
 ### Store to my_dataset for easy export below.
@@ -40,9 +53,6 @@ labels, features = targetFeatureSplit(data)
 ### http://scikit-learn.org/stable/modules/pipeline.html
 
 # Provided to give you a starting point. Try a variety of classifiers.
-from sklearn.naive_bayes import GaussianNB
-clf = GaussianNB()
-
 
 
 ### Task 5: Tune your classifier to achieve better than .3 precision and recall
@@ -56,6 +66,24 @@ clf = GaussianNB()
 from sklearn.cross_validation import train_test_split
 features_train, features_test, labels_train, labels_test = \
     train_test_split(features, labels, test_size=0.3, random_state=42)
+
+
+from sklearn.tree import DecisionTreeClassifier
+
+clf = DecisionTreeClassifier()
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+pred = clf.predict(features_test)
+print "predict time:", round(time()-t0, 3), "s"
+
+from sklearn.metrics import accuracy_score
+acc = accuracy_score(pred, labels_test)
+
+print(acc)
 
 ### Task 6: Dump your classifier, dataset, and features_list so anyone can
 ### check your results. You do not need to change anything below, but make sure
